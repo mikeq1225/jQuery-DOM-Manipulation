e186fa0304db2817aeb158baccf336e378d74c34
Michael Quarne <mikedq1225@gmail.com>
2020-02-04 17:53:37 -0800

    finished normal mode

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
-$(document).ready(function() {})
+$(document).ready(function() {
+  $(".header").on("click", function(e) {
+    $(".hidden").removeClass("content")
+    $(this)
+      .find("+ .hidden")
+      .addClass("content")
+  })
+})
+
+let html = carouselImages
+
+  .map(function(item) {
+    return `
+  <div class="imageBox">
+    <img class="images covered" src="${item.image_url}" />
+    <p class="title">${item.title}</p>
+    <p class="photographer">${item.photographer_id}</p>
+  </div>
+  `
+  })
+  .join("")
+
+document.querySelector(".carousel").innerHTML = html
